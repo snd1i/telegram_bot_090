a7f6167310bf295dd4f046c97546138611fec519
snd1i <dinokingdineking@gmail.com>
2026-01-11 02:51:51 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,15 @@
+import os
+
+# Token (Railway'dan alınacak)
+TOKEN = os.getenv("TOKEN", "")
+
+# Kanal bilgileri
+CHANNEL_LINK = "https://t.me/+wet-9MZuj044ZGQy"
+CHANNEL_ID = -1002072605977
+
+# Varsayılan dil
+DEFAULT_LANGUAGE = "en"
+
+# Bot bilgileri
+BOT_NAME = "MultiLanguage Bot"
+BOT_VERSION = "1.0"
